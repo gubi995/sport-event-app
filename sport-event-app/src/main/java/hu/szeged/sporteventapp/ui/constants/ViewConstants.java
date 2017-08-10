d932d1bfacd2784029ab8fe8e3fb01bafec45fdb
adrian.gruber <gruber950513@gmail.com>
2017-08-11 01:29:56 +0200

    Introduce view constants file

---
+++ sport-event-app/src/main/java/hu/szeged/sporteventapp/ui/constants/ViewConstants.java
@@ -0,0 +1,12 @@
+package hu.szeged.sporteventapp.ui.constants;
+
+public class ViewConstants {
+    public static final String USERNAME = "Username";
+    public static final String EMAIL = "Email";
+    public static final String PASSWORD = "Password";
+    public static final String PASSWORD_AGAIN = "Password again";
+    public static final String SIGN_UP = "Sign Up";
+    public static final String SIGN_IN = "Sign In";
+    public static final String REGISTER = "Register";
+    public static final String LOGIN = "Login";
+}
